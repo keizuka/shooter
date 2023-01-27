cd784e17eb7bcb21282da659e9a0fdde8d99ef83
keizuka <benjamin.ardilouze212@gmail.com>
2023-01-27 12:22:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ pygame.init()
 # classe qui va representer le joueur
 class Player(pygame.sprite.Sprite):
 
-    def __int__(self):
-        super().__init__()
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
         self.health = 100
         self.max_health = 100
         self.attack = 20
         self.velocity = 5
         self.image = pygame.image.load('PygameAssets-main/player.png')
         self.rect = self.image.get_rect()
-        self.rect.x = 400
+        self.rect.x = 1000
 
 
 # générer la fenetre du jeu
-pygame.display.set_caption("Hell_Fire")
-screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("SHOOTER")
+screen = pygame.display.set_mode((1080, 720))
 
 # importer l'arrier plan du jeu
 background = pygame.image.load('PygameAssets-main/bg.jpg')
@@ -32,7 +32,7 @@ running = True  # pour maintenir la fenetre ouvert
 while running:
 
     # appliquer l'arriere plan du jeu
-    screen.blit(background, (0, -350))
+    screen.blit(background, (0, -300))
 
     # appliquer l'image du joueur
     screen.blit(player.image, (0, 0))
